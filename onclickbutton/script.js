ab2dde1b56477caf3af23341206b5edb321288b3
Kshitij Mishra <kshitijmishra1302@gmail.com>
2023-09-03 11:48:14 +0530

    Mini Task 2

--- onclickbutton/script.js
+++ onclickbutton/script.js
@@ -1,3 +1,15 @@
-document.getElementById("textchanger").onclick=function(){
-    document.getElementById("texrdiv").innerHTML="Hello World, I am Kshitij ";
+let isText1 = true; 
+
+document.getElementById("textchanger").onclick = function() {
+  const textDiv = document.getElementById("texrdiv");
+  
+  if (isText1) {
+    textDiv.innerHTML = "Hello World, I am Kshitij";
+  } else {
+   
+    textDiv.innerHTML = "Hello World!!!";
+  }
+  
+  isText1 = !isText1;
 }
+
