46fe9a0b8b64e9ec1e0b2365bb87db025892ae2e
Kshitij Mishra <kshitijmishra1302@gmail.com>
2023-09-05 16:07:08 +0530

    mini task 3

---
+++ changeColorOnClick/script.js
@@ -0,0 +1,27 @@
+
+
+const btn = document.getElementById('btn');
+const btn1 = document.getElementById('btn1');
+const texrdiv = document.getElementById('texrdiv');
+
+function onClick(event) {
+  if (event.target === btn) {
+    texrdiv.classList.remove('text-yellow-700');
+    texrdiv.classList.add('text-red-400');
+    
+  } else if (event.target === btn1) {
+    texrdiv.classList.remove('text-red-400');
+    texrdiv.classList.add('text-yellow-700');
+  }
+}
+
+btn.addEventListener('click', onClick);
+btn1.addEventListener('click', onClick);
+
+
+
+
+
+
+
+
